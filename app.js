c643d12c29daa42b7f71e97d7c211535d0c407e8
javaday <javaday@gmail.com>
2016-06-14 20:41:07 -0600

    Updates

--- app.js
+++ app.js
@@ -7,7 +7,21 @@
 		var vm = this;
 
 		vm.stories = [];
+		vm.newStory = {
+			name: '',
+			details: ''
+		};
 
+		vm.addStory = function () {
+
+			var newStory = angular.copy(vm.newStory);
+
+			vm.stories = storageService.addStory(newStory);
+			
+			vm.newStory.name = '';
+			vm.newStory.details = '';
+		};	
+		
 		storageService.getStories().then(
 			function (stories) {
 				vm.stories = stories;
