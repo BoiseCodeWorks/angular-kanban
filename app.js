3aab2da1b1345d097b53b3f838910c7c4557e401
javaday <javaday@gmail.com>
2016-07-06 18:48:31 -0600

    Updated search controller

--- app.js
+++ app.js
@@ -2,27 +2,90 @@
 
 	var app = angular.module('ngKanban', ['ui.bootstrap']);
 
-	app.controller('appController', ['storageService', function (storageService) {
+	app.controller('appController', ['storageService', '$uibModal', function (storageService, $uibModal) {
 		
 		var ac = this;
 
 		ac.searchTerm = '';
 
 		ac.search = function () {
+
 			var results = storageService.findStories(ac.searchTerm);
-			console.log(results);
+			
+			var modalInstance = $uibModal.open({
+				templateUrl: 'searchResultsModal.html',
+				controller: 'searchResultsController',
+				controllerAs: 'rm',
+				resolve: {
+					results: function () {
+						return results;
+					},
+					term: function () {
+						return ac.searchTerm
+					}
+				}
+			});
+
+			modalInstance.result.then(
+				function () {
+
+				},
+				function () {
+					// cancelled
+				}
+			);			
 		};
+
+		//ac.marked = $sce.trustAsHtml(markTerms('Mr. Blue lives in a blue house and wears a blue suite and blue hat.', 'blue'));
 	}]);
 
-	app.controller('searchResultsController', ['$uibModalInstance', 'results', 'term', function ($uibModalInstance, results, term) {
+	app.controller('searchResultsController', ['$sce', '$uibModalInstance', 'results', 'term', function ($sce, $uibModalInstance, results, term) {
 		
 		var rm = this;
 
 		results = results.map(function (item) {
-			item.summary = item.summary.replace(new RegExp(term, 'gi'), )
+			item.summary = markTerms(item.summary, term);
+			item.detail = markTerms(item.detail, term);
+
+			return item;
 		});
 
 		rm.results = results;
+
+		rm.cancel = function () {
+			$uibModalInstance.dismiss('cancel');
+		};
+
+		function markTerms(text, term) {
+
+			var result = [];
+			var buffer = [];		
+			var queue = text.split('');
+
+			term = term.toLowerCase();
+
+			while (queue.length) {
+
+				buffer.push(queue.shift());
+
+				if (buffer.length === term.length) {
+					
+					var word = buffer.join('');
+
+					if (word.toLowerCase() === term) {
+						result.push('<mark>' + word + '</mark>');
+						buffer = [];
+					}
+					else {
+						result.push(buffer.shift());
+					}
+				}
+			}
+			
+			return result.join('') + buffer.join('');
+		}
+
 	}]);
+
 
 })();
