8eb2241324609ee41345873cd65d3ac5d9259e2d
javaday <javaday@gmail.com>
2016-07-07 20:40:16 -0600

    Added firebase register, log in, and log out

--- app.js
+++ app.js
@@ -2,12 +2,93 @@
 
 	var app = angular.module('ngKanban', ['ui.bootstrap']);
 
-	app.controller('appController', ['$rootScope', 'storageService', '$uibModal', function ($rootScope, storageService, $uibModal) {
+	app.value('auth', {});
+	
+	app.controller('appController', ['auth', '$rootScope', '$scope', 'storageService', 'firebaseService', '$uibModal', function (auth, $rootScope, $scope, storageService, firebaseService, $uibModal) {
 		
 		var ac = this;
 
+		ac.showRegister = true;
+		ac.showLogin = true;
+		ac.showLogout = false;
+		
+		ac.auth = {
+			user: auth.user
+		};
+
 		ac.searchTerm = '';
 
+		firebase.auth().onAuthStateChanged(function (user) {
+
+			if (user) {
+				ac.auth.user = user;
+				ac.showLogin = false;
+				ac.showRegister = false;
+				ac.showLogout = true;
+			}	
+			else {
+				ac.auth.user = null;
+				ac.showLogin = true;
+				ac.showRegister = true;
+				ac.showLogout = false;
+			}
+
+			$scope.$apply();
+		});
+
+		ac.register = function () {
+			
+			var modalInstance = $uibModal.open({
+				templateUrl: 'registerModal.html',
+				controller: 'registerModalController',
+				controllerAs: 'rm',
+				resolve: {
+					mode: function () {
+						return 'register';
+					}
+				}
+			});
+
+			modalInstance.result.then(
+				function (newUser) {
+					firebaseService.createAccount(newUser);
+				},
+				function () {
+					// cancelled
+				}
+			);			
+		};
+
+		ac.login = function () {
+			
+			var modalInstance = $uibModal.open({
+				templateUrl: 'registerModal.html',
+				controller: 'registerModalController',
+				controllerAs: 'rm',
+				resolve: {
+					mode: function () {
+						return 'login';
+					}
+				}
+			});
+
+			modalInstance.result.then(
+				function (user) {
+					firebaseService.authorizeAccount(user);
+				},
+				function () {
+					// cancelled
+				}
+			);			
+		};
+		
+		ac.logout = function () {
+			
+			if (firebase.auth().currentUser) {
+        		firebase.auth().signOut();
+      		} 
+		};
+		
 		ac.search = function () {
 
 			var results = storageService.findStories(ac.searchTerm);
@@ -41,10 +122,67 @@
 		//ac.marked = $sce.trustAsHtml(markTerms('Mr. Blue lives in a blue house and wears a blue suite and blue hat.', 'blue'));
 	}]);
 
+	app.controller('registerModalController', ['$uibModalInstance', 'mode', function ($uibModalInstance, mode) {
+		
+		var rm = this;
+		
+		rm.errors = [];
+		rm.mode = mode;
+
+		rm.user = {
+			id: '',
+			name: '',
+			email: '',
+			password: ''
+		};
+		
+		rm.register = function () {
+
+			rm.errors = [];
+			
+			if (rm.user.name === '') {
+				rm.errors.push('A name is required.');
+			}
+
+			if (rm.user.email === '') {
+				rm.errors.push('An email address is required.');
+			}
+			
+			if (rm.user.password === '') {
+				rm.errors.push('A password is required.');
+			}
+			
+			if (rm.errors.length < 1) {
+				$uibModalInstance.close(rm.user);				
+			}
+		};
+
+		rm.login = function () {
+
+			rm.errors = [];
+
+			if (rm.user.email === '') {
+				rm.errors.push('An email address is required.');
+			}
+			
+			if (rm.user.password === '') {
+				rm.errors.push('A password is required.');
+			}
+			
+			if (rm.errors.length < 1) {
+				$uibModalInstance.close(rm.user);				
+			}
+		}
+		
+		rm.cancel = function () {
+			$uibModalInstance.dismiss('cancel');
+		};
+	}]);
+	
 	app.controller('searchResultsController', ['$sce', '$uibModalInstance', 'results', 'term', function ($sce, $uibModalInstance, results, term) {
 		
 		var rm = this;
-
+		
 		rm.results = results.map(function (item) {
 
 			var resultItem = angular.copy(item);
--- services/firebase.js
+++ services/firebase.js
@@ -1,17 +1,39 @@
 (function () {
-	
+
 	var app = angular.module('ngKanban');
 
 	app.factory('firebaseService', ['$q', function ($q) {
 
+		function createAccount(user) {
+
+			firebase.auth()
+				.createUserWithEmailAndPassword(user.email, user.password)
+				.catch(function (error) {
+
+					console.log(error);
+				}
+			);
+		}
+
+		function authorizeAccount(user) {
+			firebase.auth()
+				.signInWithEmailAndPassword(user.email, user.password)
+				.catch(function (error) {
+
+					console.log(error);
+				}
+			);
+		}
 
 		function addList(list) {
-			
+
 			firebase.database().ref('lists/' + list.id).set(list);
-		}	
+		}
 
 		return {
+			createAccount: createAccount,
+			authorizeAccount: authorizeAccount,
 			addList: addList
 		};
-	}]);	
+	}]);
 })();
