ae501b82490db51ed266d6dc3e16e5caae5cee07
javaday <javaday@gmail.com>
2016-07-12 07:28:33 -0600

    Added user list component

--- services/firebase.js
+++ services/firebase.js
@@ -11,18 +11,19 @@
 				.then(function (newUser) {
 
 					newUser.updateProfile({
-  						displayName: user.name,
-  						photoURL: 'https://api.adorable.io/avatars/100/' + user.email + '.png'
+						displayName: user.name,
+						photoURL: 'https://api.adorable.io/avatars/100/' + user.email + '.png'
 					})
-					.then(
+						.then(
 						function () {
 							$rootScope.$broadcast('profile-updated', newUser);
+							$rootScope.$broadcast('userlist-updated');
 						}
-					)	
-					.catch(function (error) {
-						// TODO: broadcast error message
-						console.log(error);
-					});
+						)
+						.catch(function (error) {
+							// TODO: broadcast error message
+							console.log(error);
+						});
 				})
 				.catch(function (error) {
 					// TODO: broadcast error message
@@ -34,6 +35,9 @@
 		function authorizeAccount(user) {
 			firebase.auth()
 				.signInWithEmailAndPassword(user.email, user.password)
+				.then(function (user) {
+					addOnlineUser(user);
+				})
 				.catch(function (error) {
 					notificationService.showError('Login Failed', error.message);
 					console.log(error);
@@ -41,14 +45,60 @@
 			);
 		}
 
+		function exitAccount() {
+
+			if (firebase.auth().currentUser) {
+				removeOnlineUser(firebase.auth().currentUser);
+      		} 
+		}
+
+		function getOnlineUsers() {
+
+			var users = [];			
+			var deferred = $q.defer();
+			
+			firebase.database().ref('users/').on('value', function (snapshot) {
+
+				snapshot.forEach(function(childSnapshot) {
+      				users.push(childSnapshot.val());
+				});
+
+				deferred.resolve(users);
+			});
+
+			return deferred.promise;
+		}
+
 		function addList(list) {
 
 			firebase.database().ref('lists/' + list.id).set(list);
 		}
 
+		function addOnlineUser(user) {
+			firebase.database().ref('users/' + user.uid).set({
+				id: user.uid,
+				name: user.displayName,
+				photoUrl: user.photoURL,
+				lastLogin: new Date().getTime()
+			}).then(function () {
+				$rootScope.$broadcast('userlist-updated');
+			});
+		}
+
+		function removeOnlineUser(user) {
+			firebase.database().ref('users/' + user.uid).remove().then(
+				function () {
+					firebase.auth().signOut();
+					$rootScope.$broadcast('userlist-updated');
+				}
+			);
+		}
+
 		return {
 			createAccount: createAccount,
 			authorizeAccount: authorizeAccount,
+			exitAccount: exitAccount,
+			getOnlineUsers: getOnlineUsers,
 			addList: addList
 		};
 	}]);
